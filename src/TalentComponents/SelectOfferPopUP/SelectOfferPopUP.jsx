31bf292d2d4391b555a377fc2c9fae99e123e154
Aliislamian <aliislamian123@gmail.com>
2023-05-31 17:46:16 +0500

    updated

--- src/TalentComponents/SelectOfferPopUP/SelectOfferPopUP.jsx
+++ src/TalentComponents/SelectOfferPopUP/SelectOfferPopUP.jsx
@@ -18,16 +18,11 @@ import TalentBtn from "../TalentBtn/TalentBtn";
 import CustomOffer from "../CustomOffer/CustomOffer";
 import TalentChatPage from "../../TalentPages/TalentChatPage/TalentChatPage";
 import SelectAcceptServicePopUp from "../../components/AcceptOffer/SelectAcceptServicePopUp";
-
-
-
-
-
 const SelectOfferPopUP = (props) => {
-   var customData = '';
+  var customData = '';
   const [selectedOption, setSelectedOption] = useState('');
   const [getData, setGetData] = useState("");
-  const [post , setPost] = useState({
+  const [post, setPost] = useState({
     description: '',
     budget: '',
     delivery_time: '',
@@ -41,19 +36,19 @@ const SelectOfferPopUP = (props) => {
     }
   }, []);
 
-  const [data ,SetData ]  =useState('')
+  const [data, SetData] = useState('')
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
   };
- 
+
   const handleInput = (event) => {
-      setPost({...post, [event.target.name]: event.target.value})
+    setPost({ ...post, [event.target.name]: event.target.value })
   }
   const sendDataToDatabase = async (data) => {
     try {
       const response = await axios.post('https://linkablebackend-production-e3d2.up.railway.app/sendoffer', data);
-      console.log("=========>>>>>>>POst",response.data);
-      
+      console.log("=========>>>>>>>POst", response.data);
+
     } catch (error) {
       console.error(error);
     }
@@ -61,15 +56,15 @@ const SelectOfferPopUP = (props) => {
       const response = await axios.get('https://linkablebackend-production-e3d2.up.railway.app/sendoffer', data);
       // console.log("=========>>>>>>>get",response.data);
       const data = response.data;
-      setGetData(data);
+
       localStorage.setItem("apiData", JSON.stringify(data));
-      
-    
+      setGetData(data);
+
     } catch (error) {
       console.error(error);
     }
   };
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const data = {
@@ -82,240 +77,221 @@ const SelectOfferPopUP = (props) => {
     sendDataToDatabase(data);
   };
   const updateData = () => {
-    const newData =  getData
-    SetData (newData);
+    const newData = getData
+    SetData(newData);
   };
-  console.log("get data",getData);
+  console.log("getdata", getData);
   return (
-
-     <Dialog
+    <Dialog
       onClose={props.handleofferPopUp}
       open={props.offerPopUpState}
       className="select-offer-dialogue"
-      fullWidth={true}
-    >
+      fullWidth={true}>
       <div className="select-offer-popup-container">
- 
         <form onSubmit={handleSubmit}>
-        <div className="select-offer-popup-inner-container">
-          <div className="select-offer-cross-icon">
-            <img src={cross3} alt="cross"
-              onClick={props.handleofferPopUp}
-            />
-          </div>
-          <div
-            className="select-offer-top-heading"
-            style={{ marginTop: "5px" }}
-          >
-            Send an Offer
-          </div>
-          <div className="select-offer-sub-heading-container">
-            <div className="select-offer-select-service-heading">
-              Select Service
+          <div className="select-offer-popup-inner-container">
+            <div className="select-offer-cross-icon">
+              <img src={cross3} alt="cross"
+                onClick={props.handleofferPopUp} />
             </div>
-            <div className="select-offer-change-service-heading">Change</div>
-          </div>
-
-          {/* // */}
-          <div className="select-offer-bottom-container">
-            <div className="select-offer-gig-img-and-review-container">
-              <div className="select-offer-gig-img-div ">
-                <img src={one} alt="gig" />
+            <div
+              className="select-offer-top-heading"
+              style={{ marginTop: "5px" }}>
+              Send an Offer
+            </div>
+            <div className="select-offer-sub-heading-container">
+              <div className="select-offer-select-service-heading">
+                Select Service
               </div>
-              <div className="payment-gig-review-container">
-                <div className="select-offer-gig-heading">
-                  Develop Mobile Apps Android 
+              <div className="select-offer-change-service-heading">Change</div>
+            </div>
+            <div className="select-offer-bottom-container">
+              <div className="select-offer-gig-img-and-review-container">
+                <div className="select-offer-gig-img-div ">
+                  <img src={one} alt="gig" />
                 </div>
-                <div className="payment-gig-review-star-container">
-                  {/* <div className="order-detail-info-rating-star-div"> */}
-                  <div className="payment-rating-star-div">
-                    <StarIcon
-                      style={{ color: "gold", fontSize: "20px" }}
-                      className="payment-stars"
-                    />
-                    <StarIcon
-                      style={{ color: "gold", fontSize: "20px" }}
-                      className="payment-stars"
-                    />
-                    <StarIcon
-                      style={{ color: "gold", fontSize: "20px" }}
-                      className="payment-stars"
-                    />
-                    <StarIcon
-                      style={{ color: "gold", fontSize: "20px" }}
-                      className="payment-stars"
-                    />
-                    <StarIcon
-                      style={{ color: "gold", fontSize: "20px" }}
-                      className="payment-stars"
-                    />
+                <div className="payment-gig-review-container">
+                  <div className="select-offer-gig-heading">
+                    Develop Mobile Apps Android
                   </div>
-                  <div className="payment-rating-reviews-number">
-                    <h2 style={{ color: "black", fontSize: "1rem" }}>5 (28)</h2>
+                  <div className="payment-gig-review-star-container">
+                    {/* <div className="order-detail-info-rating-star-div"> */}
+                    <div className="payment-rating-star-div">
+                      <StarIcon
+                        style={{ color: "gold", fontSize: "20px" }}
+                        className="payment-stars" />
+                      <StarIcon
+                        style={{ color: "gold", fontSize: "20px" }}
+                        className="payment-stars" />
+                      <StarIcon
+                        style={{ color: "gold", fontSize: "20px" }}
+                        className="payment-stars"
+                      />
+                      <StarIcon
+                        style={{ color: "gold", fontSize: "20px" }}
+                        className="payment-stars"
+                      />
+                      <StarIcon
+                        style={{ color: "gold", fontSize: "20px" }}
+                        className="payment-stars"
+                      />
+                    </div>
+                    <div className="payment-rating-reviews-number">
+                      <h2 style={{ color: "black", fontSize: "1rem" }}>5 (28)</h2>
+                    </div>
+                    {/* </div> */}
                   </div>
-                  {/* </div> */}
+                  <div className="select-offer-starting-price-heading">
+                    From $10
+                  </div>
                 </div>
-                <div className="select-offer-starting-price-heading">
-                  From $10
+              </div>
+            </div>
+            <div className="select-offer-description-container">
+              Offer Description
+            </div>
+            <div className="select-offer-text-area-container">
+              <textarea
+                className="select-offer-textarea"
+                placeholder="project delivery, deadlines, requirements etc. "
+                name="description"
+                id="offerDescription"
+                cols="30"
+                rows="8"
+                maxLength={1000}
+                onChange={handleInput}>
+              </textarea>
+              <span className="select-offer-word-count">1000</span>
+            </div>
+
+            <div className="select-offer-details-container">
+              <div className="select-offer-details-left-container">
+                <img
+                  src={budget}
+                  alt="icon"
+                  className="select-offer-details-icon"
+                />
+                <div className="select-offer-details-text">Total Budget</div>
+              </div>
+              <div className="select-offer-details-right-container">
+
+                <input type="number" min="0" placeholder="$" name="budget" onChange={handleInput} />
+              </div>
+            </div>
+            <div className="select-offer-details-container">
+              <div className="select-offer-details-left-container">
+                <img
+                  src={responseTime}
+                  alt="icon"
+                  className="select-offer-details-icon"
+                />
+                <div className="select-offer-details-text">Delivery Time</div>
+              </div>
+              <div className="select-offer-details-right-container">
+                <input type="number" min="0" placeholder="Days" name="delivery_time" onChange={handleInput} />
+              </div>
+                </div>
+            <div className="select-offer-details-container">
+              <div className="select-offer-details-left-container">
+                <img
+                  src={revisionIcon}
+                  alt="icon"
+                  className="select-offer-details-icon"/>
+                <div className="select-offer-details-text">Revisions</div>
+              </div>
+              <div className="select-offer-details-right-container" onChange={handleInput}>
+                <select onChange={handleOptionChange} name="revisions">
+                  <option value="Select Days"></option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </select>
+              </div>
+            </div>
+            <div className="select-offer-details-container">
+              <div className="select-offer-details-left-container">
+                <img
+                  src={service}
+                  alt="icon"
+                  className="select-offer-details-icon"
+                />
+                <div className="select-offer-details-text">Services</div>
+              </div>
+              <div className="select-offer-details-right-container" onChange={handleInput}>
+                <select onChange={handleOptionChange} name="services">
+                  <option value="Select Days"></option>
+                  <option value="App">App</option>
+                  <option value="web">Web</option>
+                  <option value="AI">AI</option>
+                  <option value="IOT">IOT</option>
+                </select>
+              </div>
+            </div>
+            <div className="select-offer-details-container">
+              <div className="select-offer-details-left-container">
+                <img
+                  src={requestRequirement}
+                  alt="icon"
+                  className="select-offer-details-icon"
+                />
+                <div className="select-offer-details-text">
+                  Request Requirments
                 </div>
               </div>
-            </div>
-          </div>
-
-          <div className="select-offer-description-container">
-            Offer Description
-          </div>
-          <div className="select-offer-text-area-container">
-            <textarea
-              className="select-offer-textarea"
-              placeholder="project delivery, deadlines, requirements etc. "
-              name="description"
-              id="offerDescription"
-              cols="30"
-              rows="8"
-              maxLength={1000}
-
-              onChange={handleInput}
-            ></textarea>
-            <span className="select-offer-word-count">1000</span>
-          </div>
-
-          <div className="select-offer-details-container">
-            <div className="select-offer-details-left-container">
-              <img
-                src={budget}
-                alt="icon"
-                className="select-offer-details-icon"
-              />
-              <div className="select-offer-details-text">Total Budget</div>
-            </div>
-            <div className="select-offer-details-right-container">
-              
-              <input type="number" min="0" placeholder="$" name="budget" onChange={handleInput}  />
-            </div>
-          </div>
-          <div className="select-offer-details-container">
-            <div className="select-offer-details-left-container">
-              <img
-                src={responseTime}
-                alt="icon"
-                className="select-offer-details-icon"
-              />
-              <div className="select-offer-details-text">Delivery Time</div>
-            </div>
-            <div className="select-offer-details-right-container">
-              <input type="number" min="0" placeholder="Days" name="delivery_time"  onChange={handleInput} />
-            </div>
-
-            
-          </div>
-          <div className="select-offer-details-container">
-            <div className="select-offer-details-left-container">
-              <img
-                src={revisionIcon}
-                alt="icon"
-                className="select-offer-details-icon"
-              />
-            
-
-              <div className="select-offer-details-text">Revisions</div>
-            </div>
-            <div className="select-offer-details-right-container" onChange={handleInput}>
-              <select onChange={handleOptionChange} name="revisions">
-              <option value="Select Days"></option>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-            </select>
-            </div>
-          </div>
-          <div className="select-offer-details-container">
-            <div className="select-offer-details-left-container">
-              <img
-                src={service}
-                alt="icon"
-                className="select-offer-details-icon"
-              />
-              <div className="select-offer-details-text">Services</div>
-            </div>
-            <div className="select-offer-details-right-container" onChange={handleInput}>
-              <select onChange={handleOptionChange} name="services">
-              <option value="Select Days"></option>
-        <option value="App">App</option>
-        <option value="web">Web</option>
-        <option value="AI">AI</option>
-        <option value="IOT">IOT</option>
-            </select>
-            </div>
-          </div>
-          <div className="select-offer-details-container">
-            <div className="select-offer-details-left-container">
-              <img
-                src={requestRequirement}
-                alt="icon"
-                className="select-offer-details-icon"
-              />
-              <div className="select-offer-details-text">
-                Request Requirments
+              <div className="select-offer-details-right-container">
+                <label class="toggle">
+                  <input class="toggle-checkbox" type="checkbox" />
+                  <div class="toggle-switch"></div>
+                </label>{" "}
               </div>
             </div>
-            <div className="select-offer-details-right-container">
-              <label class="toggle">
-                <input class="toggle-checkbox" type="checkbox" />
-                <div class="toggle-switch"></div>
-              </label>{" "}
-            </div>
-          </div>
-          <div className="select-offer-details-container">
-            <div className="select-offer-details-left-container">
-              <img
-                src={offerExp}
-                alt="icon"
-                className="select-offer-details-icon"
-              />
-              <div className="select-offer-details-text">
-                Offer Expiration (Opt)
+            <div className="select-offer-details-container">
+              <div className="select-offer-details-left-container">
+                <img
+                  src={offerExp}
+                  alt="icon"
+                  className="select-offer-details-icon"
+                />
+                <div className="select-offer-details-text">
+                  Offer Expiration (Opt)
+                </div>
+              </div>
+              <div className="select-offer-details-right-container">
+                <label class="toggle">
+                  <input class="toggle-checkbox" type="checkbox" />
+                  <div class="toggle-switch"></div>
+                </label>{" "}
               </div>
             </div>
-            <div className="select-offer-details-right-container">
-              <label class="toggle">
-                <input class="toggle-checkbox" type="checkbox" />
-                <div class="toggle-switch"></div>
-              </label>{" "}
-            </div>
-          </div>
-          <div className="select-offer-details-container">
-            <div className="select-offer-details-left-container">
-              <img
-                src={autoSave}
-                alt="icon"
-                className="select-offer-details-icon"
-              />
-              <div className="select-offer-details-text">
-                Save for future offers
+            <div className="select-offer-details-container">
+              <div className="select-offer-details-left-container">
+                <img
+                  src={autoSave}
+                  alt="icon"
+                  className="select-offer-details-icon"
+                />
+                <div className="select-offer-details-text">
+                  Save for future offers
+                </div>
+              </div>
+              <div className="select-offer-details-right-container">
+                <label class="toggle">
+                  <input class="toggle-checkbox" type="checkbox" />
+                  <div class="toggle-switch"></div>
+                </label>{" "}
               </div>
             </div>
-            <div className="select-offer-details-right-container">
-              <label class="toggle">
-                <input class="toggle-checkbox" type="checkbox" />
-                <div class="toggle-switch"></div>
-              </label>{" "}
+            <div className="select-offer-btn-container">
+              {data && <CustomOffer data={getData} />}
+              <SelectAcceptServicePopUp data={getData} />
+              <button type="submit" className="bg-[#47966B] hover:bg-[#47966B] text-white font-bold py-2 px-4 rounded">
+                Submit
+              </button>
             </div>
           </div>
-          <div className="select-offer-btn-container">
-         {data && <CustomOffer data={getData} />} 
-          <SelectAcceptServicePopUp data={getData} />
- 
-  
-  <button type="submit" className="bg-[#47966B] hover:bg-[#47966B] text-white font-bold py-2 px-4 rounded">
-    Submit
-  </button>
-</div>
-          
-        </div>
-   </form>
+        </form>
       </div>
     </Dialog>
   );
--- src/components/AcceptOffer/SelectAcceptServicePopUp.jsx
+++ src/components/AcceptOffer/SelectAcceptServicePopUp.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../AcceptOffer/Acceptoffer.css"
 import cross3 from "../../images/cross3.png";
 import one from "../../images/one.png";
@@ -9,12 +9,8 @@ import tickGreen from "../../images/tickGreen.png"
 import orderUpdate from "../../images/orderUpdate.png"
 import greenclock from "../../images/greenclock.png"
 
-const SelectAcceptServicePopUp = (props) => {
-  // const {description, budget, delivery_time, revisions, services} = props;
-  // console.log("propspropsporps", description);
-  console.log('=====asdasd=>>',props);
-
-  var result = Object.keys(props).map((key) => [key, props[key]]);
+const SelectAcceptServicePopUp = ( props ) => {
+        console.log("propsssssss",props.data)
   return (
     <>
       <Dialog
@@ -23,58 +19,54 @@ const SelectAcceptServicePopUp = (props) => {
         onClose={props.handleAcceptservicePopup}
         fullScreen={true}
         fullWidth={true}>
-                {
-                  result.map((item, index) => (
-                  
-                    <div className="Main" key={index}>
-                <div className="info">
-                  <img src={profilepic} alt="" />
-                  <div className="info_1">
-                    <h3>Me</h3>
-                    <p className="infoparagrapgh">jun 03 ,4:30PM</p>
-                  </div>
-                </div>
-                <div className="card">
-                  <div className="card1">
-                    <h1 className="card1h1">Budget</h1>
-                    <span className="card1span">$ {console.log(result)}</span>
-                  </div>
-                  <hr />
-                  <div className="card2">
-                    <p className="card2p">Contrary to popular belief, word in classical literature</p>
-                  </div>
-                  <hr />
-                  <div className="card3">
-                    <h1 className="card3h1">Your offer include</h1>
-                    <div className="spandiv">
-                      <img className="spanimage" src={greenclock} alt="" />
-                      <span className="card3span">2 day revision</span>
-                      <img className="spanimage" src={greenclock} alt="" />
-                      <span className="card3span">2 day delivery</span>
-                      <img className="spanimage" src={tickGreen} alt="" />
-                      <span className="card3span">Service</span>
-                    </div>
-                  </div>
-                  <hr />
-                  <div className="Expireoffer">
-                    <span className="Expireoffespan">offer expire on 2 jun 20233</span>
-                    <div className="Expireofferbtn">
-                      <button className="vieworderbtn">
-                        view order
-                      </button>
-                      <button className="Acceptofferbtn">
-                        offer  Accepted
-                      </button>
-                    </div>
-                    <hr />
-                  </div>
-                </div>
+        <div className="Main">
+          <div className="info">
+            <img src={profilepic} alt="" />
+            <div className="info_1">
+              <h3>Me</h3>
+              <p className="infoparagrapgh">jun 03 ,4:30PM  </p>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card1">
+              <h1 className="card1h1">i will write seo optimized content</h1>
+              <span className="card1span">$ 40</span>
+            </div>
+            <hr />
+            <div className="card2">
+              <p className="card2p">Contrary to popular belief, word in classical literature</p>
+            </div>
+            <hr />
+            <div className="card3">
+              <h1 className="card3h1">Your offer include</h1>
+              <div className="spandiv">
+                <img className="spanimage" src={greenclock} alt="" />
+                <span className="card3span">2 day revision</span>
+                <img className="spanimage" src={greenclock} alt="" />
+                <span className="card3span">2 day delivery</span>
+                <img className="spanimage" src={tickGreen} alt="" />
+                <span className="card3span">Service</span>
               </div>
-                  ))
-                }
+            </div>
+            <hr />
+            <div className="Expireoffer">
+              <span className="Expireoffespan">offer expire on 2 jun 20233</span>
+              <div className="Expireofferbtn">
+                <button className="vieworderbtn">
+                  view order
+                </button>
+                <button className="Acceptofferbtn">
+                  offer  Accepted
+                </button>
+              </div>
+              <hr />
+            </div>
+          </div>
+        </div>
       </Dialog>
     </>
   );
 };
 
 export default SelectAcceptServicePopUp;
+
