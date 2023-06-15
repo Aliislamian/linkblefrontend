369f2f7259d47d642555e079a305e536154336c5
Aliislamian <aliislamian123@gmail.com>
2023-06-16 01:52:08 +0500

    updated

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 const newRequest = axios.create({
   // baseURL: "http://localhost:5000",
-  baseURL: "https://linkeble.herokuapp.com",
+  baseURL: "https://linkablebackend-production-e3d2.up.railway.app",
   withCredentials: true,
 });
 export default newRequest;
 
 
-export const baseURL = "https://linkeble.herokuapp.com";
+export const baseURL = "https://linkablebackend-production-e3d2.up.railway.app";
