9c5558a3514cbe5cafaeb99401e960645948bcc0
Aliislamian <aliislamian123@gmail.com>
2023-07-12 21:45:36 +0500

    Updates

---
+++ precache-manifest.75c2e8ecc3c585959179af3c8319e733.js
@@ -0,0 +1,278 @@
+self.__precacheManifest = [
+  {
+    "revision": "327aa4edfad03ac90cb0",
+    "url": "/linkblefrontend/static/css/main.fe43058c.chunk.css"
+  },
+  {
+    "revision": "327aa4edfad03ac90cb0",
+    "url": "/linkblefrontend/static/js/main.2359a89b.chunk.js"
+  },
+  {
+    "revision": "f1bc0c4f7d6185441ba7",
+    "url": "/linkblefrontend/static/js/runtime~main.75634476.js"
+  },
+  {
+    "revision": "cef65e6ad4d998911ef6",
+    "url": "/linkblefrontend/static/css/2.427d6151.chunk.css"
+  },
+  {
+    "revision": "cef65e6ad4d998911ef6",
+    "url": "/linkblefrontend/static/js/2.f590dca5.chunk.js"
+  },
+  {
+    "revision": "355f7509382d4da0c827",
+    "url": "/linkblefrontend/static/js/3.9029de37.chunk.js"
+  },
+  {
+    "revision": "b4b1569cd44d5c3b789c1b9faebb5e16",
+    "url": "/linkblefrontend/static/media/one.b4b1569c.png"
+  },
+  {
+    "revision": "bf1a5c301841c6af113da414b1d093cc",
+    "url": "/linkblefrontend/static/media/circularprofile.bf1a5c30.png"
+  },
+  {
+    "revision": "e9ca92fe7806026faa3f8c5f2bbf733f",
+    "url": "/linkblefrontend/static/media/tabprofileimg1.e9ca92fe.png"
+  },
+  {
+    "revision": "e6dcb52e5a0c27f5eb800c4452682ca9",
+    "url": "/linkblefrontend/static/media/logo.e6dcb52e.png"
+  },
+  {
+    "revision": "69a3433ebe582bbf6796de9712d438ff",
+    "url": "/linkblefrontend/static/media/tabprofileimg3.69a3433e.png"
+  },
+  {
+    "revision": "27472c83e060041d56efec76378822ae",
+    "url": "/linkblefrontend/static/media/two.27472c83.png"
+  },
+  {
+    "revision": "3f4b1bece7325bf2d6b28b5ea26d289c",
+    "url": "/linkblefrontend/static/media/search.3f4b1bec.png"
+  },
+  {
+    "revision": "38eb4434b14b241c6cc8d0920e753862",
+    "url": "/linkblefrontend/static/media/submitcardimg1.38eb4434.png"
+  },
+  {
+    "revision": "c7b36117847d6a21c53bdafe8b95ff78",
+    "url": "/linkblefrontend/static/media/avatar.c7b36117.jpg"
+  },
+  {
+    "revision": "38d9292fbb5f6b50c91d230d3a495e88",
+    "url": "/linkblefrontend/static/media/Artificial.38d9292f.png"
+  },
+  {
+    "revision": "529119ec87ed37019f852348578b9d8b",
+    "url": "/linkblefrontend/static/media/BlockChain.529119ec.png"
+  },
+  {
+    "revision": "ed50d5aa9ecf43f22fc5f7d3bedddd3c",
+    "url": "/linkblefrontend/static/media/three.ed50d5aa.png"
+  },
+  {
+    "revision": "077915fb84435216e54a21d005a53a2b",
+    "url": "/linkblefrontend/static/media/four.077915fb.png"
+  },
+  {
+    "revision": "968ad58f801a998fcd8c07412178a913",
+    "url": "/linkblefrontend/static/media/submitcardimg3.968ad58f.png"
+  },
+  {
+    "revision": "4c6af49adedf0e6c6c2511694db235aa",
+    "url": "/linkblefrontend/static/media/ProgrammingServices.4c6af49a.png"
+  },
+  {
+    "revision": "7e437456273fed26e7a237f462b7156f",
+    "url": "/linkblefrontend/static/media/five.7e437456.png"
+  },
+  {
+    "revision": "c84a81b3017ab17e8df2cbc5c66b2e68",
+    "url": "/linkblefrontend/static/media/MachineLearning.c84a81b3.png"
+  },
+  {
+    "revision": "b8107e26518b18a41f8ca282b6f0a162",
+    "url": "/linkblefrontend/static/media/Database.b8107e26.png"
+  },
+  {
+    "revision": "4cd84a13f4578597400737ffc0631f33",
+    "url": "/linkblefrontend/static/media/tabcardimg1.4cd84a13.png"
+  },
+  {
+    "revision": "d278a71d54de2f9845837e2e50c4e03b",
+    "url": "/linkblefrontend/static/media/submitcardimg2.d278a71d.png"
+  },
+  {
+    "revision": "bf11a7637c96ce9308e7343717b59a56",
+    "url": "/linkblefrontend/static/media/earningGraph.bf11a763.PNG"
+  },
+  {
+    "revision": "b2e926bc6c72107d9fa7af66406d1b9c",
+    "url": "/linkblefrontend/static/media/tabcardimg2.b2e926bc.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/linkblefrontend/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/linkblefrontend/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/linkblefrontend/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/linkblefrontend/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "7ef6f07067bda0864363eee677da783c",
+    "url": "/linkblefrontend/static/media/Ayaz.7ef6f070.png"
+  },
+  {
+    "revision": "71d25d54158274a1804f0c1c8b9e8458",
+    "url": "/linkblefrontend/static/media/ultimateCard.71d25d54.png"
+  },
+  {
+    "revision": "bbed89955b638f74adcfac87ac234427",
+    "url": "/linkblefrontend/static/media/six.bbed8995.png"
+  },
+  {
+    "revision": "22f58f6dad9520f57df3b1fd8d72ba7f",
+    "url": "/linkblefrontend/static/media/seven.22f58f6d.png"
+  },
+  {
+    "revision": "5dd37fa782f7d65b857f6e18bfadee81",
+    "url": "/linkblefrontend/static/media/noOrders.5dd37fa7.png"
+  },
+  {
+    "revision": "83c1ba9380434c2c9c79c7841858c023",
+    "url": "/linkblefrontend/static/media/Level.83c1ba93.png"
+  },
+  {
+    "revision": "b38186985bd416a1fa55cd53fcb53f6d",
+    "url": "/linkblefrontend/static/media/levelBar.b3818698.PNG"
+  },
+  {
+    "revision": "c630ef3c5740bc7e1061ad6f2d0c055b",
+    "url": "/linkblefrontend/static/media/greenclock.c630ef3c.png"
+  },
+  {
+    "revision": "39e81df5e08221e1f001dcd3c94de801",
+    "url": "/linkblefrontend/static/media/Group1.39e81df5.png"
+  },
+  {
+    "revision": "b11bf6c7fc1dca758a3d19b5636d7076",
+    "url": "/linkblefrontend/static/media/Group2.b11bf6c7.png"
+  },
+  {
+    "revision": "17103fdf617eab3ac018795c69c5639f",
+    "url": "/linkblefrontend/static/media/joinnowimage.17103fdf.png"
+  },
+  {
+    "revision": "bc7b516cfa3b2bcddbda6bc248e4b60c",
+    "url": "/linkblefrontend/static/media/freelancer.bc7b516c.png"
+  },
+  {
+    "revision": "0398d204c3c765ec1a4cb900e1fc3da2",
+    "url": "/linkblefrontend/static/media/eight.0398d204.png"
+  },
+  {
+    "revision": "17316241f64b610e529fcef7e53d3a3e",
+    "url": "/linkblefrontend/static/media/nine.17316241.png"
+  },
+  {
+    "revision": "f34f63923bbddfae6cd97d7fc0cb193e",
+    "url": "/linkblefrontend/static/media/ten.f34f6392.png"
+  },
+  {
+    "revision": "eddecd60637fca35fad8344b1b2c784b",
+    "url": "/linkblefrontend/static/media/eleven.eddecd60.png"
+  },
+  {
+    "revision": "72598530fd190dbcfe5e7c2e79e6d70d",
+    "url": "/linkblefrontend/static/media/twelve.72598530.png"
+  },
+  {
+    "revision": "ddd6b89fb8ef9fe02f31d8fc9bb04965",
+    "url": "/linkblefrontend/static/media/thirteen.ddd6b89f.png"
+  },
+  {
+    "revision": "4ea5187b232bff90dafcefb32d2e56b4",
+    "url": "/linkblefrontend/static/media/fourteen.4ea5187b.png"
+  },
+  {
+    "revision": "f243cb7a260b5ee2cf5fdaab1b05a8d8",
+    "url": "/linkblefrontend/static/media/fifteen.f243cb7a.png"
+  },
+  {
+    "revision": "594f4d9d3187f454b9061bf152a507a0",
+    "url": "/linkblefrontend/static/media/sliderimage.594f4d9d.png"
+  },
+  {
+    "revision": "11198be16a99d609f5f779cc0cd0aa9e",
+    "url": "/linkblefrontend/static/media/tabcardimg3.11198be1.png"
+  },
+  {
+    "revision": "46fb9ec0d9dc058730ff9567460f019f",
+    "url": "/linkblefrontend/static/media/cloud.46fb9ec0.PNG"
+  },
+  {
+    "revision": "21426b780aad000a6917147b5d21800e",
+    "url": "/linkblefrontend/static/media/fvrtone.21426b78.png"
+  },
+  {
+    "revision": "693db120568ac5b64c6b65cc90acfdec",
+    "url": "/linkblefrontend/static/media/fvrtthree.693db120.png"
+  },
+  {
+    "revision": "48b505d18491ec98ccc6482bcca1790a",
+    "url": "/linkblefrontend/static/media/milstone.48b505d1.PNG"
+  },
+  {
+    "revision": "8f3e1bf7d64f72e07fa7ee48f2eb2538",
+    "url": "/linkblefrontend/static/media/CreditCard2.8f3e1bf7.png"
+  },
+  {
+    "revision": "2f63c39240cd19760bcacba2891e9f2f",
+    "url": "/linkblefrontend/static/media/CreditCard1.2f63c392.png"
+  },
+  {
+    "revision": "8a24075130aea7cbb6fb3f4dab8d541d",
+    "url": "/linkblefrontend/static/media/accountSecurity.8a240751.PNG"
+  },
+  {
+    "revision": "2aff7e3a61ab1d5b150629115ca47346",
+    "url": "/linkblefrontend/static/media/nft.2aff7e3a.png"
+  },
+  {
+    "revision": "3232919d2b51ad8b0e0a14226991dca5",
+    "url": "/linkblefrontend/static/media/postedJobImg.3232919d.png"
+  },
+  {
+    "revision": "1ca3eb0843ce629c1f93fb2198c1d04f",
+    "url": "/linkblefrontend/static/media/gigGraph.1ca3eb08.PNG"
+  },
+  {
+    "revision": "796d77ad90a77578dd305d60accf4ee2",
+    "url": "/linkblefrontend/static/media/step6.796d77ad.png"
+  },
+  {
+    "revision": "02208df380fd3128e4b417a573af8575",
+    "url": "/linkblefrontend/static/media/SuccessFactor.02208df3.png"
+  },
+  {
+    "revision": "68c2c87e900f4ba81bbdebe64cef72a8",
+    "url": "/linkblefrontend/static/media/AboutImg.68c2c87e.png"
+  },
+  {
+    "revision": "68ff8e4c72424def1cfecda0630552d9",
+    "url": "/linkblefrontend/static/media/payapl.68ff8e4c.png"
+  },
+  {
+    "revision": "b3101012ad87fb5efe7505f4d8a2ebab",
+    "url": "/linkblefrontend/index.html"
+  }
+];
